7c028e79ff01be2b378c8d386480ba3022e96595
Chase W <71685891+ChaseWestlake@users.noreply.github.com>
2021-11-06 19:39:53 -0700

    Updates for image display from AI and UDP

--- UserContent/Views/MenuView.xaml.cs
+++ UserContent/Views/MenuView.xaml.cs
@@ -10,16 +10,43 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
 namespace ModernGUI_Surveilia.UserContent.Views
 {
-    /// <summary>
-    /// Interaction logic for MenuView.xaml
-    /// </summary>
+    /*
+     *  Written By:     Chase Westlake
+     *  
+     *  Description:    This code currently reads an image in a directory and updates the former image to the new one.
+     *  
+     *                  This needs to be able to respond to UDP flags and upload variable images (newest) from the Surveilia UDP images. 
+     * 
+     * This documentation is used to upload new images to the GUI
+     * https://docs.microsoft.com/en-us/dotnet/api/system.windows.media.imaging.bitmapimage?redirectedfrom=MSDN&view=windowsdesktop-5.0 
+     * 
+     */
     public partial class MenuView : UserControl
     {
+
         public MenuView()
         {
             InitializeComponent();
+        }
+
+
+        private void ViewImg_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapImage bi = new BitmapImage();
+
+            bi.BeginInit();
+            
+            //This is the Directory, change it**********************************
+            bi.UriSource = new Uri(@"D:\School\Year3\GroundStation\GUIupD\GUI_UpdatedFM-main\Images\Surveilia.jpg");
+            
+            
+            bi.EndInit();
+
+            HumanRecogImg.Source = bi;
+
         }
     }
 }
--- UserContent/Views/StatsView.xaml.cs
+++ UserContent/Views/StatsView.xaml.cs
@@ -131,6 +131,7 @@ namespace ModernGUI_Surveilia.UserContent.Views
                 {
                     Title = "Graph1",
                     Values = new ChartValues<int>()
+
                 },
 
                 new LineSeries
@@ -139,6 +140,14 @@ namespace ModernGUI_Surveilia.UserContent.Views
                     Values = new ChartValues<int>(),
                     PointGeometry = null
                 },
+
+                new LineSeries
+                {
+                    Title = "Graph3",
+                    Values = new ChartValues<int> { 1, 2, 400, 250, 250, 250, 250, 300, 100, 10 },
+                    LineSmoothness = 0,
+                    PointGeometry = DefaultGeometries.Circle
+                }
             };
 
             for (int i = 0; i < listIndex; i++)
@@ -152,22 +161,9 @@ namespace ModernGUI_Surveilia.UserContent.Views
             }
 
 
-            Labels = new[] { "1", "2", "3", "4", "5", "6", "7"};
+            //Labels = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13" };
             YFormatter = value => value.ToString("F");
-
-            //modifying the series collection will animate and update the chart
-           /* SeriesCollection.Add(new LineSeries
-            {
-                Title = "Series 4",
-                Values = new ChartValues<double> { 5, 3, 2, 4 },
-                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
-                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
-                PointGeometrySize = 50,
-                PointForeground = Brushes.Gray
-            });
-           */
-
-
+            XFormatter = values => values.ToString("F");
 
 
 
@@ -201,5 +197,6 @@ namespace ModernGUI_Surveilia.UserContent.Views
         public SeriesCollection SeriesCollection { get; set; }
         public string[] Labels { get; set; }
         public Func<double, string> YFormatter { get; set; }
+        public Func<double, string> XFormatter { get; set; }
     }
 }
